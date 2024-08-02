7ace0eb6a89122f7977d40832f7907b0614f096c
John <meyrzhan04.04@mail.ru>
2024-08-02 14:03:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// function fun1() {
+//     var rtl = document.getElementById('rtl').value;
+//     var rtr = document.getElementById('rtr').value;
+
+//     var rbr = document.getElementById('rbr').value;
+//     var rbl = document.getElementById('rbl').value;
+
+//     var ttl = document.getElementById('ttl');
+//     var ttr = document.getElementById('ttr');
+    
+//     var tbr = document.getElementById('tbr');
+//     var tbl = document.getElementById('tbl');
+
+//     var block = document.getElementById('block');
+
+//     ttl.value=rtl;
+//     ttr.value=rtr;
+//     tbr.value=rtr;
+//     tbl.value=rbl;
+
+//     block.style.borderRadius = rtl+'px  '  +rtr +'px  ' +rbr +'px '+rbl +'px';
+
+// }
+
+
+// function iop() {
+//     var op = document.getElementById('opo')
+//     op.classList.add('pol')
+    
+// }
+
